c9d9698279f3693cbed9edb0abab02f5c7f4dc33
centralhardware <alex@centralhardware.ru>
2020-10-15 18:52:03 +0700

    fix rubocop warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,68 +1,71 @@
 require 'bundler/setup'
 Bundler.require
 
-class Mongo::Client
-  class MockCollection
-    # rubocop:disable Style/ClassVars
-    @@correct = true
-    @@written = false
+module Mongo
+  class Client
+    class MockCollection
+      include Singleton
 
-    def find(*)
-      [{ 'listId' => 1, 'swimlaneId' => 2 }]
-    end
-
-    def insert_one(element)
-      @@written = true
-      unless element[:text].nil?
-        if !element[:boardId].eql?('12') ||
-           !element[:cardId].eql?('13') ||
-           !element[:userId].eql?('1')  ||
-           !element[:text].eql?('text text')
-          @@correct = false
-        end
+      def find(*)
+        [{ 'listId' => 1, 'swimlaneId' => 2 }]
       end
-      unless element[:activityType].nil?
-        if !element[:userId].eql?('1') ||
-           !element[:activityType].eql?('addComment') ||
-           !element[:boardId].eql?('12') ||
-           !element[:cardId].eql?('13') ||
-           !element[:listId].eql?(1) ||
-           !element[:swimlaneId].eql?(2)
-          @@correct = false
-        end
+
+      def insert_one(element)
+        @written = true
+        @correct = if !element[:text].nil?
+                     comment_correct?(element)
+                   else
+                     activity_correct?(element)
+                   end
+      end
+
+      def correct?
+        @correct
+      end
+
+      def written?
+        @written
+      end
+
+      def reset!
+        @correct = true
+        @written = false
+      end
+
+      private
+
+      def comment_correct?(element)
+        element[:boardId].eql?('12') &&
+          element[:cardId].eql?('13') &&
+          element[:userId].eql?('1')  &&
+          element[:text].eql?('text text')
+      end
+
+      def activity_correct?(element)
+        element[:userId].eql?('1') &&
+          element[:activityType].eql?('addComment') &&
+          element[:boardId].eql?('12') &&
+          element[:cardId].eql?('13') &&
+          element[:listId].eql?(1) &&
+          element[:swimlaneId].eql?(2)
       end
     end
 
-    def correct?
-      @@correct
+    def self.new(*)
+      {
+        cards: MockCollection.instance,
+        activity: MockCollection.instance,
+        card_comments: MockCollection.instance
+      }
     end
-
-    def written?
-      @@written
-    end
-
-    def reset!
-      @@correct = true
-      @@written = false
-    end
-  end
-
-  def self.new(*)
-    {
-      cards: MockCollection.new,
-      activity: MockCollection.new,
-      card_comments: MockCollection.new
-    }
   end
 end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
